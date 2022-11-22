bee90447ec64ab57a5d1360378070ac010a6d503
Sergey Rubnikovich <sergeyrubnikovich@yandex.ru>
2022-11-22 19:27:59 +0700

    Default avatar

--- db/crudUsersAvatars.go
+++ db/crudUsersAvatars.go
@@ -15,7 +15,13 @@ type UserAvatar struct {
 
 func GetUsersAvatarsByIds(ids []int) (usersAvatars []UserAvatar) {
 	usersIds := strings.Trim(strings.Join(strings.Fields(fmt.Sprint(ids)), ","), "[]")
+	var unknownIds = map[int]bool{}
+	for userId, _ := range usersIds {
+		unknownIds[userId] = false
+	}
+
 	rows, err := db.Query(fmt.Sprintf("SELECT user_id, src_path FROM %s WHERE user_id IN (%s);", UserAvatarTableName, usersIds))
+
 	if err != nil {
 		err := rows.Close()
 		if err != nil {
@@ -36,7 +42,21 @@ func GetUsersAvatarsByIds(ids []int) (usersAvatars []UserAvatar) {
 			return nil
 		}
 		usersAvatars = append(usersAvatars, userAvatar)
+		unknownIds[userAvatar.UserId] = true
 	}
+
+	for userId, _ := range usersIds {
+		if unknownIds[userId] == false {
+			userAvatar := UserAvatar{}
+			userAvatar.UserId = userId
+			userAvatar.SrcPath = ""
+			if err != nil {
+				return nil
+			}
+			usersAvatars = append(usersAvatars, userAvatar)
+		}
+	}
+
 	log.Println(usersAvatars)
 	j, _ := json.Marshal(&usersAvatars)
 	log.Println(string(j))
